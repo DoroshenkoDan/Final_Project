d81a309996ffd17a0fbb49b134864e91626f0629
DoroshenkoDan <doroshenkowin@gmail.com>
2023-10-01 17:56:22 +0300

    add products-container on HomePage

--- client/src/Redux/api/index.js
+++ client/src/Redux/api/index.js
@@ -1,9 +1,10 @@
+
 export default async function getProducts() {
-  try {
-    const data = await fetch(process.env.PUBLIC_URL + '/products.json')
-    const products = await data.json()
-    return products
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const data = await fetch(process.env.PUBLIC_URL + '/products.json')
+        const products = await data.json()
+        return products
+    } catch (e) {
+        console.log(e)
+    }
 }
