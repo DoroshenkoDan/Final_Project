832b1674968e5233d9765a75d1819496a9f67681
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2023-10-03 20:15:04 +0300

    "add changes to config files"

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'Сюда копируем URI для доступа в БД, например: mongodb+srv://username:password@..........................',
+    'mongodb+srv://clientstore182023:tHpnKTLKbP8m2NQC@cluster0.pufccq2.mongodb.net/store?retryWrites=true&w=majority&appName=AtlasApp',
   secretOrKey: 'random very very secret string',
 };
