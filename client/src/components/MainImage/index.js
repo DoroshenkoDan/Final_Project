ec15e54e369fa6ea924c5ed70ba3473db21a19ba
Sidar08 <sidar80639900474@gmail.com>
2023-10-04 17:03:44 +0300

    Add small size

--- client/src/components/MainImage/index.js
+++ client/src/components/MainImage/index.js
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './MainImage.module.scss'
 
-function MainImage () {
-    return (
-      <div className={styles.container}>
-        <div className={styles.banner}>
-          <div className={styles.text}>
-            <h2>
-              The furniture brand for the
-              <br />
-              future, with timeless designs
-            </h2>
-            <button className={styles.button}>View collection</button>
-            <p>
-              A new era in eco friendly furniture with Avelon, the French luxury
-              retail brand with nice fonts, tasteful colors and a beautiful way
-              to display things digitally using modern web technologies
-            </p>
-          </div>
-          <div className={styles.image}>
-            <img src="/img/baner/main.jpg" alt="Main" />
-          </div>
+function MainImage() {
+        const [size, setSize] = useState('large')
+
+        useEffect(() => {
+          const handleResize = () => {
+            if (window.innerWidth <= 481) {
+              setSize('small')
+            } else if (window.innerWidth <= 912) {
+              setSize('medium')
+            } else {
+              setSize('large')
+            }
+          }
+
+          window.addEventListener('resize', handleResize)
+          handleResize() 
+
+          return () => window.removeEventListener('resize', handleResize)
+        }, [])
+  return (
+    <div className={styles.container}>
+      <div className={styles.banner} data-size={size}>
+        <div className={styles.text}>
+          <h2>
+            The furniture brand for the
+            <br />
+            future, with timeless designs
+          </h2>
+          <button className={styles.button}>View collection</button>
+          <p>
+            A new era in eco friendly furniture with Avelon, the French luxury
+            retail brand with nice fonts, tasteful colors and a beautiful way to
+            display things digitally using modern web technologies
+          </p>
+        </div>
+        <div className={styles.image}>
+          <img src="/img/baner/main.jpg" alt="Main" />
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default MainImage
