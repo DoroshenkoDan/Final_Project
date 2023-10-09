a45caa0dadc8e98a7780afd7183256f7a1e9723e
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-10-09 17:55:21 +0300

    change HomePage

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import Header from '../components/Header'
-import { Footer } from '../components/Footer/Footer'
+import {Footer} from '../components/Footer/Footer'
 
 export default function Layout() {
     return (
         <>
             <header>
-                <Header />
+                <Header/>
             </header>
             <main className="container">
-                <Outlet />
+                <Outlet/>
             </main>
-            <Footer />
+            <Footer/>
         </>
     )
 }
