0ae89ee9d25c1b9085bdad0f5b7977a7168f088c
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-10-09 17:36:51 +0300

    change HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react'
-// import ProductsContainer from '../components/ProductsContainer'
-import ProductPage from './ProductPage'
+import ProductsContainer from '../components/ProductsContainer'
+// import ProductPage from './ProductPage'
 export default function HomePage() {
     return (
         <>
-            <ProductPage />
-            {/* <ProductsContainer /> */}
-
+             <ProductsContainer />
         </>
     )
 }
