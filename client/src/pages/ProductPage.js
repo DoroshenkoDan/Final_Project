75a6174244966e8602ad855f3e49dc3e77c40b53
DoroshenkoDan <doroshenkowin@gmail.com>
2023-10-16 21:52:52 +0300

    fix product Page&Container

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -3,11 +3,11 @@ import ProductItem from '../components/ProductItem'
 import ProductsContainer from '../components/ProductsContainer'
 import { useParams } from 'react-router-dom'
 export default function ProductPage() {
-  const { productId } = useParams()
-  return (
-    <>
-      <ProductItem props={productId} />
-      <ProductsContainer category="Ceramics" id={productId} />
-    </>
-  )
+    const { productId } = useParams()
+    return (
+        <>
+            <ProductItem props={productId} />
+            <ProductsContainer id={productId} />
+        </>
+    )
 }
