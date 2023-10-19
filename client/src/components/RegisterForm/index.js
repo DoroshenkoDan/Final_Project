04967355008de9ed7f1ae9ad8e58dd953923b870
Volodymyr Medvedchuk <vladmeeedy@gmail.com>
2023-10-19 12:54:33 +0300

    merge loginPage

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -38,7 +38,7 @@ export default function OrderForm() {
                 firstName: Yup.string().max(25, 'Must be 25 characters or less').min(2, 'Must be more than 1 characters').required('Firstname is required').matches(/^[^\p{P}\p{S}\d]+$/u, 'Invalid firstname format'),
                 lastName: Yup.string().max(25, 'Must be 25 characters or less').min(2, 'Must be more than 1 character').required('Lastname is required').matches(/^[^\p{P}\p{S}\d]+$/u, 'Invalid lastname format'),
                 login: Yup.string().max(10, 'Login must be between 3 and 10 characters').min(3, 'Login must be between 3 and 10 characters').matches(/^[a-zA-Z0-9]+$/, 'Invalid login format').required('Email is required'),
-                email: Yup.string().matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Invalid email format').required('Email is required'),
+                email: Yup.string().matches(/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Invalid email format').required('Email is required'),
                 password: Yup.string().matches(/^[a-zA-Z0-9]+$/, 'Allowed characters for password is a-z, A-Z, 0-9.').required('Email is required').max(30, 'Password must be between 7 and 30 characters').min(7, 'Password must be between 7 and 30 characters'),
                 telephone: Yup.string().matches(/^\+380\d{3}\d{2}\d{2}\d{2}$/, 'That is not a valid phone number').required('Mobile is required'),
             })}>
