a0322966799d0d0b300887a50621b45aef537ada
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-10-22 19:01:50 +0300

    add auth from LocalStorage

--- client/src/components/Token/index.js
+++ client/src/components/Token/index.js
@@ -9,7 +9,6 @@ export const setAuthToken = token => {
     if (token) {
         axios.defaults.headers.common.Authorization = token;
     } else {
-        // Delete auth header
         delete axios.defaults.headers.common.Authorization;
     }
 };
