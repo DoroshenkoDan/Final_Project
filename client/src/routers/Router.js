39572f34c8007c7415e39a4306b7e25602a08323
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-10-23 12:16:13 +0300

    check

--- client/src/routers/Router.js
+++ client/src/routers/Router.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Navigate, useRoutes, BrowserRouter } from 'react-router-dom'
 
 import Layout from '../layouts/Layout'
-import HomePage from '../pages/HomePage'
+import HomePage from '../pages/HomePage/HomePage'
 import CartPage from '../pages/CartPage'
-import FavoritesPage from '../pages/FavoritesPage'
-import CatalogPage from '../pages/CatalogPage'
-import CategoryPage from '../pages/CategoryPage'
-import ProductPage from '../pages/ProductPage'
-import AuthPage from "../pages/AuthPage";
+import FavoritesPage from '../pages/FavoritesPage/FavoritesPage'
+import CategoryPage from '../pages/CategoryPage/CategoryPage'
+import ProductPage from '../pages/ProductPage/ProductPage'
+import AuthPage from "../pages/AuthPage/AuthPage";
+import AllProductsPage from "../pages/AllProductsPage/AllProductsPage";
 
 
 
@@ -24,7 +24,7 @@ function Routes() {
                 { path: 'cart/', element: <CartPage /> },
                 { path: 'favorites/', element: <FavoritesPage /> },
                 { path: 'login/', element: <AuthPage /> },
-                { path: 'catalog/', element: <CatalogPage /> },
+                { path: 'allProducts/', element: <AllProductsPage /> },
                 { path: 'category/:categoryId', element: <CategoryPage /> },
                 { path: 'products/:productId', element: <ProductPage /> },
             ],
