e9526207ae57ec18b13edbfc0716edbf424f7a59
DoroshenkoDan <doroshenkowin@gmail.com>
2023-10-25 01:02:27 +0300

    hieta

--- client/src/Redux/reducers/cartReducer.js
+++ client/src/Redux/reducers/cartReducer.js
@@ -1,66 +1,84 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  cart: [],
-  cartStatus: false,
+    cart: [],
+    cartStatus: false,
 }
 
 export const cartReducer = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    addToCart(state, action) {
-      const itemToAdd = action.payload
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart(state, action) {
+            const itemToAdd = action.payload
 
-      const existingItem = state.cart.find((item) => item._id === itemToAdd._id)
-      if (existingItem) {
-        existingItem.Quantity += itemToAdd.Quantity
-      } else {
-        state.cart.push(itemToAdd)
-      }
-    },
-    removeFromCart(state, action) {
-      const idToRemove = action.payload
-      const itemIndex = state.cart.findIndex((item) => item._id === idToRemove)
-      if (itemIndex !== -1) {
-        state.cart.splice(itemIndex, 1)
-      }
-    },
-    incrementQuantity(state, action) {
-      const id = action.payload
+            const existingItem = state.cart.find((item) => item._id === itemToAdd._id)
+            if (existingItem) {
+                existingItem.Quantity += itemToAdd.Quantity
+            } else {
+                state.cart.push(itemToAdd)
+            }
+        },
+        removeFromCart(state, action) {
+            const idToRemove = action.payload
+            const itemIndex = state.cart.findIndex((item) => item._id === idToRemove)
+            if (itemIndex !== -1) {
+                state.cart.splice(itemIndex, 1)
+            }
+        },
+        incrementQuantity(state, action) {
+            const id = action.payload
 
-      const product = state.cart.find((item) => item._id === id)
-      if (product) {
-        product.prodQuantity += 1
-      }
-    },
-    dicrementQuantity(state, action) {
-      const id = action.payload
+            const product = state.cart.find((item) => item._id === id)
+            if (product) {
+                product.prodQuantity += 1
+            }
+        },
+        dicrementQuantity(state, action) {
+            const id = action.payload
 
-      const product = state.cart.find((item) => item._id === id)
-      if (product && product.prodQuantity > 1) {
-        product.prodQuantity = product.prodQuantity - 1
-      }
+            const product = state.cart.find((item) => item._id === id)
+            if (product && product.prodQuantity > 1) {
+                product.prodQuantity = product.prodQuantity - 1
+            }
+        },
+        clearCart(state, action) {
+            state.cart = []
+        },
+        openCartForm(state) {
+            state.status = true
+        },
+        closeCartForm(state) {
+            state.status = false
+        }, addArrayToCart(state, action) {
+            const itemsToAdd = Array.isArray(action.payload) ? action.payload : [];
+
+            // Проверяем, что itemsToAdd является массивом
+            if (!Array.isArray(itemsToAdd)) {
+                return;
+            }
+
+            // Фильтруем элементы, чтобы исключить дубликаты по _id
+            const uniqueItemsToAdd = itemsToAdd.filter(itemToAdd =>
+                !state.cart.some(existingItem => existingItem._id === itemToAdd._id)
+            );
+
+            // Добавляем уникальные элементы в существующий массив cart
+            state.cart = [...state.cart, ...uniqueItemsToAdd];
+        },
+
+
     },
-    clearCart(state, action) {
-      state.cart = []
-    },
-    openCartForm(state) {
-      state.status = true
-    },
-    closeCartForm(state) {
-      state.status = false
-    },
-  },
 })
 
 export default cartReducer.reducer
 export const {
-  addToCart,
-  removeFromCart,
-  incrementQuantity,
-  dicrementQuantity,
-  clearCart,
-  openCartForm,
-  closeCartForm,
+    addToCart,
+    removeFromCart,
+    incrementQuantity,
+    dicrementQuantity,
+    addArrayToCart,
+    clearCart,
+    openCartForm,
+    closeCartForm,
 } = cartReducer.actions
--- client/src/pages/CartPage/index.js
+++ client/src/pages/CartPage/index.js
@@ -1,137 +1,164 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import CartProductList from '../../components/CartProductList/index.js'
 import styles from './CartPage.module.scss'
-import { useSelector } from 'react-redux'
+// eslint-disable-next-line
+import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import { HOST } from '../../components/Token'
+// eslint-disable-next-line
+import { addArrayToCart } from '../../Redux/reducers/cartReducer.js'
 export default function Cart() {
-  const cartReducer = useSelector((state) => state.store.cart.cart)
-  const allProducts = useSelector((state) => state.products.data)
-  const userStatus = useSelector((state) => state.store.user.status)
+    const cartReducer = useSelector((state) => state.store.cart.cart)
+    const allProducts = useSelector((state) => state.products.data)
+    const userStatus = useSelector((state) => state.store.user.status)
+    // eslint-disable-next-line
+    const dispatch = useDispatch()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (userStatus) {
-        try {
-          const dataExist = await getCart()
-          console.log('DataStatus', dataExist.data)
-          if (dataExist.data !== null) {
-            await updateServerCart()
-          } else {
-            await createServerCart()
-          }
-        } catch (error) {
-          console.error('Error fetching cart data:', error)
+    useEffect(() => {
+        if (userStatus) {
+            getChekCart()
+            console.log("getChekCart-worked");
         }
-      } else {
-        console.log('User is not logged in')
-      }
+
+    }, [userStatus])
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (userStatus) {
+                try {
+                    const dataExist = await getCart()
+                    console.log('DataStatus', dataExist.data)
+                    if (dataExist.data !== null) {
+                        await updateServerCart()
+                    } else {
+                        await createServerCart()
+                    }
+                } catch (error) {
+                    console.error('Error fetching cart data:', error)
+                }
+            } else {
+                console.log('User is not logged in')
+            }
+        }
+        fetchData()
+    }, [cartReducer, userStatus])
+
+    async function getCart() {
+        const response = await axios.get(HOST + '/cart')
+        console.log('cartGet', response)
+        return response
     }
-    fetchData()
-  }, [cartReducer, userStatus])
 
-  async function getCart() {
-    const response = await axios.get(HOST + '/cart')
-    console.log('cartGet', response)
-    return response
-  }
+    async function getChekCart() {
+        const response = await axios.get(HOST + '/cart')
+        if (response && response.data && response.data.products) {
+            dispatch(addArrayToCart(response.data.products))
+            console.log('cartCheckGet', response)
+        } else {
+            console.error(' getChekCart: объект или свойство products отсутствуют');
+        }
 
-  async function updateServerCart() {
-    const arrayToSend = { products: cartReducer }
-    axios
-      .put(HOST + '/cart', arrayToSend)
-      .then((updatedCart) => {
-        console.log(arrayToSend)
-        console.log(updatedCart)
-        console.log('All is Good Object sended')
-      })
-      .catch((err) => {
-        console.log('твой код хуйня', err)
-      })
-  }
-
-  async function createServerCart() {
-    const arrayToSend = { products: cartReducer }
-    axios
-      .post(HOST + '/cart', arrayToSend)
-      .then((updatedCart) => {
-        console.log(arrayToSend)
-        console.log(updatedCart)
-        console.log('All is Good Object sended')
-      })
-      .catch((err) => {
-        console.log('твой код хуйня', err)
-      })
-  }
-
-  function mergeObjectsWithSameId(array1, array2) {
-    const mergedObjects = []
-
-    for (const obj1 of array1) {
-      const matchingObject = array2.find((obj2) => obj2._id === obj1._id)
-      if (matchingObject) {
-        mergedObjects.push({ ...obj1, ...matchingObject })
-      }
+        return response
     }
-    return mergedObjects
-  }
 
-  const cartProducts = mergeObjectsWithSameId(cartReducer, allProducts)
-  console.log('CartReducer, ProductReducer', cartReducer, allProducts)
+    async function updateServerCart() {
+        const arrayToSend = { products: cartReducer }
+        axios
+            .put(HOST + '/cart', arrayToSend)
+            .then((response) => {
 
-  const totalCurrentPrice = cartProducts.reduce((total, product) => {
-    const productValue = product.currentPrice * product.prodQuantity
-    return total + productValue
-  }, 0)
+                console.log('(Update) All is Good Object sended', response)
+            })
+            .catch((err) => {
+                console.log('твой код хуйня', err)
+            })
+    }
 
-  if (cartProducts.length === 0) {
-    return <p>loading...</p>
-  }
+    async function createServerCart() {
+        const arrayToSend = { products: cartReducer }
+        axios
+            .post(HOST + '/cart', arrayToSend)
+            .then(() => {
 
-  return (
-    <div className={styles['cart-container']}>
-      <h3 className={styles['cart-tittle-welcome']}>Your shopping cart</h3>
-      <div className={styles['cart-section-names']}>
-        <p>Product</p>
-        <p>Quantity</p>
-        <p>Price</p>
-      </div>
-      <div className={styles['cart-list-container']}>
-        {cartProducts.map((product) => (
-          <CartProductList
-            key={product.id}
-            img={product.imageUrls}
-            name={product.name}
-            quantity={product.prodQuantity}
-            price={product.currentPrice}
-            discribe={product.description}
-            id={product._id}
-          />
-        ))}
-      </div>
-      <div className={styles['cart-total-and-order-btn-container']}>
-        <p className={styles['cart-total-price']}>
-          Subtotal
-          <span
-            style={{
-              color: '#2A254B',
-              fontSize: '24px',
-              padding: '0px 0px 0px 15px',
-            }}
-          >
-            ${totalCurrentPrice}
-          </span>
-        </p>
-        <p>Taxes and shipping are calculated at checkout</p>
-        <button
-          onClick={() => {
-            getCart()
-          }}
-          className={styles['cart-order-btn']}
-        >
-          Go to checkout
-        </button>
-      </div>
-    </div>
-  )
+                console.log('(Create)All is Good Object sended')
+            })
+            .catch((err) => {
+                console.log('твой код хуйня', err)
+            })
+    }
+
+    function mergeObjectsWithSameId(array1, array2) {
+        const mergedObjects = []
+
+        for (const obj1 of array1) {
+            const matchingObject = array2.find((obj2) => obj2._id === obj1._id)
+            if (matchingObject) {
+                mergedObjects.push({ ...obj1, ...matchingObject })
+            }
+        }
+        return mergedObjects
+    }
+
+    const cartProducts = mergeObjectsWithSameId(cartReducer, allProducts)
+    console.log('CartReducer, ProductReducer', cartReducer, allProducts)
+
+    const totalCurrentPrice = cartProducts.reduce((total, product) => {
+        const productValue = product.currentPrice * product.prodQuantity
+        return total + productValue
+    }, 0)
+
+    if (cartProducts.length === 0) {
+        return <div>
+            <p>Cart</p>
+            <p>Your cart is empty </p>
+        </div>
+    }
+
+    return (
+        <div className={styles['cart-container']}>
+            <h3 className={styles['cart-tittle-welcome']}>Your shopping cart</h3>
+            <div className={styles['cart-section-names']}>
+                <p>Product</p>
+                <p>Quantity</p>
+                <p>Price</p>
+            </div>
+            <div className={styles['cart-list-container']}>
+                {cartProducts.map((product) => (
+                    <CartProductList
+                        key={product.id}
+                        img={product.imageUrls}
+                        name={product.name}
+                        quantity={product.prodQuantity}
+                        price={product.currentPrice}
+                        discribe={product.description}
+                        id={product._id}
+                    />
+                ))}
+            </div>
+            <div className={styles['cart-total-and-order-btn-container']}>
+                <p className={styles['cart-total-price']}>
+                    Subtotal
+                    <span
+                        style={{
+                            color: '#2A254B',
+                            fontSize: '24px',
+                            padding: '0px 0px 0px 15px',
+                        }}
+                    >
+                        ${totalCurrentPrice}
+                    </span>
+                </p>
+                <p>Taxes and shipping are calculated at checkout</p>
+                <button
+                    onClick={() => {
+                        getCart()
+                    }}
+                    className={styles['cart-order-btn']}
+                >
+                    Go to checkout
+                </button>
+            </div>
+        </div>
+    )
 }
