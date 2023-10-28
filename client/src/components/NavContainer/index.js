78f9837e16557449223086815ba28a6a4dd13d78
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-10-29 00:56:00 +0300

    merge dev fix header

--- client/src/components/NavContainer/index.js
+++ client/src/components/NavContainer/index.js
@@ -1,64 +1,51 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import styles from './NavContainer.module.scss'
-import { NavLink } from 'react-router-dom'
-import { fetchCategories } from '../../Redux/reducers/categoriesReducers'
+import {NavLink} from 'react-router-dom'
+import {fetchCategories} from '../../Redux/reducers/categoriesReducers'
 import PropTypes from 'prop-types'
 
 export default function NavContainer(props) {
-  const categories = useSelector((state) => state.categories.categories)
-  const dispatch = useDispatch()
+    const categories = useSelector((state) => state.categories.categories)
+    const dispatch = useDispatch()
 
-  function getCategories() {
-    dispatch(fetchCategories())
-  }
+    function getCategories() {
+        dispatch(fetchCategories())
+    }
 
-  useEffect(() => {
-    getCategories()
-  }, [])
+    useEffect(() => {
+        getCategories()
+    }, [])
 
-  return (
-    <nav
-      className={`${styles.nav} ${props.isMenuHidden ? styles.navDisplay : ''}`}
-    >
-      <ul className={styles.navList}>
-        <li className={`${styles.navItem} ${styles.navItemDisplay}`}>
-          <NavLink className={styles.navLink} to="/cart/">
-            Cart
-          </NavLink>
-        </li>
-        <li className={`${styles.navItem} ${styles.navItemDisplay}`}>
-          <NavLink className={styles.navLink} to="/favorites/">
-            Favorites
-          </NavLink>
-        </li>
-        {/* {categories &&
-          categories.length > 0 &&
-          categories.map((category) => (
-            <li key={category.id} className={styles.navItem}>
-              <NavLink
-                to={`/category/${category.id}/`}
-                className={styles.navLink}
-              >
-                {category.name}
-              </NavLink>
-            </li>
-          ))} */}
-        {categories.map((category) => (
-          <li key={category.id} className={styles.navItem}>
-            <NavLink
-              to={`/category/${category.id}/`}
-              className={styles.navLink}
-            >
-              {category.name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  )
+    return (
+        <nav
+            className={`${styles.nav} ${props.isMenuHidden ? styles.navDisplay : ''}`}
+        >
+            <ul className={styles.navList}>
+                <NavLink className={`${styles.navLink} ${styles.navItemDisplay}`} to="/cart/">
+                    <li className={styles.navItem}>
+                        Cart
+                    </li>
+                </NavLink>
+                <NavLink className={`${styles.navLink} ${styles.navItemDisplay}`} to="/favorites/">
+                    <li className={`${styles.navItem} ${styles.navItemDisplay}`}>
+                        Favorites
+                    </li>
+                </NavLink>
+                {categories.map((category) => (
+                    <NavLink key={category.id}
+                             to={`/category/${category.id}/`}
+                             className={styles.navLink}>
+                        <li className={styles.navItem}>
+                            {category.name}
+                        </li>
+                    </NavLink>
+                ))}
+            </ul>
+        </nav>
+    )
 }
 
 NavContainer.propTypes = {
-  isMenuHidden: PropTypes.bool.isRequired,
+    isMenuHidden: PropTypes.bool.isRequired,
 }
