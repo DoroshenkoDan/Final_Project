0ad28a5a0855b6d53ea1471853dd96db95f52ce8
Name SurnameLarysa <lorik280319@gmail.com>
2023-11-12 17:55:58 +0200

    add tests

--- client/src/components/Toast/index.js
+++ client/src/components/Toast/index.js
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import CloseBtnIcon from "../Icons/CloseBtnIcon";
-import styles from "./Toast.module.scss";
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import CloseBtnIcon from '../Icons/CloseBtnIcon'
+import styles from './Toast.module.scss'
+import SuccessIcon from '../Icons/SuccessIcon'
 
-export default function Toast({ message, duration = 2500, onClose }) {
-  const [isVisible, setIsVisible] = useState(true);
-  const [progress, setProgress] = useState(100);
+export default function Toast({ message, duration = 2300, onClose }) {
+  const [isVisible, setIsVisible] = useState(true)
+  const [progress, setProgress] = useState(100)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prevProgress) => prevProgress - 100 / (duration / 100));
-    }, 100);
+      setProgress((prevProgress) => prevProgress - 100 / (duration / 100))
+    }, 100)
 
     const timer = setTimeout(() => {
-      clearInterval(interval);
-      setIsVisible(false);
-      onClose && onClose();
-    }, duration);
+      clearInterval(interval)
+      setIsVisible(false)
+      onClose && onClose()
+    }, duration)
 
     return () => {
-      clearInterval(interval);
-      clearTimeout(timer);
-    };
-  }, [duration, onClose]);
+      clearInterval(interval)
+      clearTimeout(timer)
+    }
+  }, [duration, onClose])
 
   return isVisible ? (
     <div className={styles.toast}>
@@ -33,14 +34,22 @@ export default function Toast({ message, duration = 2500, onClose }) {
       >
         <CloseBtnIcon></CloseBtnIcon>
       </span>
-      <p className={styles.toast__message}>{message}</p>
-      <div className={styles.toast__progress__bar} style={{ width: `${progress}%` }} />
+      <div className={styles.toast__message}>
+        <div className={styles.toast__message__icon}>
+          <SuccessIcon></SuccessIcon>
+        </div>
+        <p className={styles.toast__message__text}>{message}</p>
+      </div>
+      <div
+        className={styles.toast__progress__bar}
+        style={{ width: `${progress}%` }}
+      />
     </div>
-  ) : null;
+  ) : null
 }
 
 Toast.propTypes = {
   message: PropTypes.string,
   duration: PropTypes.number,
   onClose: PropTypes.func,
-};
+}
